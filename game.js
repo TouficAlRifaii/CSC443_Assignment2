375a3ece923d2e3320a0c0afead0c627b4448431
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-09-18 22:54:10 +0300

    finishing the best local login system

--- game.js
+++ game.js
@@ -1,68 +1,78 @@
-window.onload = () => {
-  const startElement = document.getElementById("start");
-  const boundaries = document.getElementsByClassName("boundary");
-  const endElement = document.getElementById("end");
-  const statusH2 = document.getElementById("status");
-  const game = document.getElementById("game");
-  const scoreElement = document.getElementById("score");
-  const user = JSON.parse(
-    localStorage.getItem(localStorage.getItem("currentUser"))
-  );
-  let gameState = 0;
-  let score = user["score"];
-  scoreElement.innerHTML = "Score: " + score;
-  function losing() {
-    if (gameState == 1) {
-      for (let i = 0; i < boundaries.length; i++) {
-        boundaries[i].style.backgroundColor = "#ff8888";
+if (localStorage.getItem("currentUser") == "null") {
+  location.assign("index.html");
+} else {
+  window.onload = () => {
+    const startElement = document.getElementById("start");
+    const boundaries = document.getElementsByClassName("boundary");
+    const endElement = document.getElementById("end");
+    const statusH2 = document.getElementById("status");
+    const game = document.getElementById("game");
+    const scoreElement = document.getElementById("score");
+    const logout = document.getElementById("logout");
+    const user = JSON.parse(
+      localStorage.getItem(localStorage.getItem("currentUser"))
+    );
+    let gameState = 0;
+    let score = user["score"];
+    scoreElement.innerHTML = "Score: " + score;
+    function losing() {
+      if (gameState == 1) {
+        for (let i = 0; i < boundaries.length - 1; i++) {
+          boundaries[i].style.backgroundColor = "#ff8888";
+        }
+        statusH2.innerHTML = "You Lose!";
+        gameState = 0;
+        score -= 10;
+        scoreElement.innerHTML = "Score: " + score;
+        user["score"] = score;
+        localStorage.setItem(user["username"], JSON.stringify(user));
       }
-      statusH2.innerHTML = "You Lose!";
-      gameState = 0;
-      score -= 10;
-      scoreElement.innerHTML = "Score: " + score;
-      user["score"] = score;
-      localStorage.setItem(user["username"], JSON.stringify(user));
     }
-  }
-  function winning() {
-    if (gameState == 1) {
-      statusH2.innerHTML = "You Win!";
-      gameState = 0;
-      score += 5;
-      scoreElement.innerHTML = "Score: " + score;
-      user["score"] = score;
-      localStorage.setItem(user["username"], JSON.stringify(user));
-    }
-  }
-  function preventCheating() {
-    if (gameState == 1) {
-      for (let i = 0; i < boundaries.length; i++) {
-        boundaries[i].style.backgroundColor = "#ff8888";
+    function winning() {
+      if (gameState == 1) {
+        statusH2.innerHTML = "You Win!";
+        gameState = 0;
+        score += 5;
+        scoreElement.innerHTML = "Score: " + score;
+        user["score"] = score;
+        localStorage.setItem(user["username"], JSON.stringify(user));
       }
-      statusH2.innerHTML =
-        "The game is restarted without affecting your score!";
-      gameState = 0;
     }
-  }
+    function preventCheating() {
+      if (gameState == 1) {
+        for (let i = 0; i < boundaries.length - 1; i++) {
+          boundaries[i].style.backgroundColor = "#ff8888";
+        }
+        statusH2.innerHTML =
+          "The game is restarted without affecting your score!";
+        gameState = 0;
+      }
+    }
 
-  game.addEventListener("mouseleave", () => preventCheating());
-  startElement.addEventListener("mouseenter", () => {
-    if (gameState == 0) {
-      gameState = 1;
-      for (let i = 0; i < boundaries.length; i++) {
-        boundaries[i].style.backgroundColor = "#eeeeee";
+    game.addEventListener("mouseleave", () => preventCheating());
+    startElement.addEventListener("mouseenter", () => {
+      if (gameState == 0) {
+        gameState = 1;
+        for (let i = 0; i < boundaries.length - 1; i++) {
+          boundaries[i].style.backgroundColor = "#eeeeee";
+        }
+        statusH2.innerHTML = "Reach The end without touching the walls!";
+        console.log(gameState);
+        for (let i = 0; i < boundaries.length; i++) {
+          boundaries[i].addEventListener("mouseenter", () => losing());
+        }
+        endElement.addEventListener("mouseenter", () => winning());
       }
-      statusH2.innerHTML = "Reach The end without touching the walls!";
-      console.log(gameState);
-      for (let i = 0; i < boundaries.length; i++) {
-        boundaries[i].addEventListener("mouseenter", () => losing());
-      }
-      endElement.addEventListener("mouseenter", () => winning());
-    }
-  });
-  startElement.addEventListener("click", () => {
-    user["score"] = 0;
-    localStorage.setItem(user["username"], JSON.stringify(user));
-    location.reload();
-  });
-};
+    });
+    startElement.addEventListener("click", () => {
+      user["score"] = 0;
+      localStorage.setItem(user["username"], JSON.stringify(user));
+      location.reload();
+    });
+
+    logout.addEventListener("click", () => {
+      localStorage.setItem("currentUser", null);
+      location.assign("index.html");
+    });
+  };
+}
--- login.js
+++ login.js
@@ -1,40 +1,48 @@
-window.onload = () => {
-  const usernameField = document.getElementById("username");
-  const passwordField = document.getElementById("password");
-  const submitButton = document.getElementById("submit");
-  const storage = window.localStorage ; 
-  const h3 = document.getElementById("DoesNot");
-  let flag = false;
-  submitButton.addEventListener("click" , ()=>{
-
-    const username = usernameField.value;
-    if (username == ""){
-      flag = true;
+if (localStorage.getItem("currentUser")== "null") {
+  window.onload = () => {
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
+    const submitButton = document.getElementById("submit");
+    const storage = window.localStorage ; 
+    const h3 = document.getElementById("DoesNot");
+    let flag = false;
+    submitButton.addEventListener("click" , ()=>{
+  
+      const username = usernameField.value;
+      if (username == ""){
+        flag = true;
+      }
+      const password = passwordField.value;
+      if (password == ""){
+        flag = true;
+      }
+      if (flag == false){
+      const user = JSON.parse(storage.getItem(username))
+      if( user == null){
+          h3.innerHTML = "User does not exist";
+  
+      }
+      else{
+          if(user["password"] == password){
+              storage.setItem("currentUser" , username);
+              location.assign("game.html");
+          }
+          else{
+              h3.innerHTML = "Wrong Password";
+          }
+  
+      }
     }
-    const password = passwordField.value;
-    if (password == ""){
-      flag = true;
+    else {
+      h3.innerHTML = "Both inputs are required";
     }
-    if (flag == false){
-    const user = JSON.parse(storage.getItem(username))
-    if( user == null){
-        h3.innerHTML = "User does not exist";
+  
+    })
+  };
+} else
+{
+  location.assign("gamee.html");
 
-    }
-    else{
-        if(user["password"] == password){
-            storage.setItem("currentUser" , username);
-            location.assign("game.html");
-        }
-        else{
-            h3.innerHTML = "Wrong Password";
-        }
+}
 
-    }
-  }
-  else {
-    h3.innerHTML = "Both inputs are required";
-  }
 
-  })
-};
