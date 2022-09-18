b8ecb01babf9c7b8ea50be286669f914d108ebec
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-09-18 23:54:39 +0300

    typo 2

--- login.js
+++ login.js
@@ -1,48 +1,37 @@
-if (localStorage.getItem("currentUser")== "null") {
+if (localStorage.getItem("currentUser") == "null") {
   window.onload = () => {
     const usernameField = document.getElementById("username");
     const passwordField = document.getElementById("password");
     const submitButton = document.getElementById("submit");
-    const storage = window.localStorage ; 
+    const storage = window.localStorage;
     const h3 = document.getElementById("DoesNot");
     let flag = false;
-    submitButton.addEventListener("click" , ()=>{
-  
+    submitButton.addEventListener("click", () => {
       const username = usernameField.value;
-      if (username == ""){
+      if (username == "") {
         flag = true;
       }
       const password = passwordField.value;
-      if (password == ""){
+      if (password == "") {
         flag = true;
       }
-      if (flag == false){
-      const user = JSON.parse(storage.getItem(username))
-      if( user == null){
+      if (flag == false) {
+        const user = JSON.parse(storage.getItem(username));
+        if (user == null) {
           h3.innerHTML = "User does not exist";
-  
-      }
-      else{
-          if(user["password"] == password){
-              storage.setItem("currentUser" , username);
-              location.assign("game.html");
+        } else {
+          if (user["password"] == password) {
+            storage.setItem("currentUser", username);
+            location.assign("game.html");
+          } else {
+            h3.innerHTML = "Wrong Password";
           }
-          else{
-              h3.innerHTML = "Wrong Password";
-          }
-  
+        }
+      } else {
+        h3.innerHTML = "Both inputs are required";
       }
-    }
-    else {
-      h3.innerHTML = "Both inputs are required";
-    }
-  
-    })
+    });
   };
-} else
-{
+} else {
   location.assign("game.html");
-
 }
-
-
